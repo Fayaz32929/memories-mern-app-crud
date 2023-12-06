9520cf31b3f84bb7d954bd60623ad0eb21bba7b2
fayaz <lagharifayazali3@gmail.com>
2023-12-07 01:16:45 +0500

    change url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://memories-app-api.vercel.app/api/posts";
+const url = "https://memories-api-nu.vercel.app/api";
 export const fetchPosts = () => axios.get(url);
 
 export const createPost = (newPost) => axios.post(url, newPost);
