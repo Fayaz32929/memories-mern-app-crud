d45944d2b0bcc1465900b29469058092334e751b
fayaz <lagharifayazali3@gmail.com>
2023-12-07 00:29:45 +0500

    final chages

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/api", PostRoutes);
+app.use("/api/posts", PostRoutes);
+app.get("/", (req, res) => {
+ res.send("hello to mmemories api");
+});
 
 const PORT = process.env.PORT || 5000;
 
